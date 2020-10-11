ef68c36105e89f4beb8cd55fca6af85bd33f3f81
Miln Blaz <milan@vivant.eco>
2020-10-11 19:37:52 +0200

    init

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import colors from './app/config/colors';
 
@@ -7,13 +7,40 @@ import Screen from './app/components/Screen';
 import LoginScreen from './app/screens/LoginScreen';
 
 import Test from './app/test/Test';
+import AppText from './app/components/AppText';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <AppText>Tweets</AppText>
+    <Button
+      title='View Tweet bruh'
+      onPress={() => navigation.navigate('TweetDetails')}
+    />
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <AppText>TweetDetails</AppText>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
-    <Screen style={styles.container}>
-      {/* <LoginScreen /> */}
-      <Test />
-    </Screen>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
